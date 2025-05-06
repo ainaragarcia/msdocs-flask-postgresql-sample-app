fc082179521c4aa7e0f0edb9a011e9c9de0e39cf
ainaragarcia <99136789+ainaragarcia@users.noreply.github.com>
2025-05-06 12:50:47 +0000

    añadido html

--- models.py
+++ models.py
@@ -1,11 +1,37 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import validates
 from datetime import datetime
-from extensions import db
+from extensions import db  
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurant'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    street_address = Column(String(50))
+    description = Column(String(250))
+
+    def __str__(self):
+        return self.name
+
+class Review(db.Model):
+    __tablename__ = 'review'
+    id = Column(Integer, primary_key=True)
+    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
+    user_name = Column(String(30))
+    rating = Column(Integer)
+    review_text = Column(String(500))
+    review_date = Column(DateTime)
+
+    @validates('rating')
+    def validate_rating(self, key, value):
+        assert value is None or (1 <= value <= 5)
+        return value
+
+    def __str__(self):
+        return f"{self.user_name}: {self.review_date:%x}"
 
 class Imagen(db.Model):
     __tablename__ = 'imagenes'
-
     id = Column(Integer, primary_key=True)
     username = Column(String(100))
     filename = Column(String(100))
